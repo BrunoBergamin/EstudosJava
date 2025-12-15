e09233ce6896b0538df67360afa4f2088fed6e10
BrunoBergamin <246331198+BrunoBergamin@users.noreply.github.com>
2025-12-15 18:42:44 -0300

    Desafio concluido

--- OOP Exercícios/DesafioCampeao/src/Main.java
+++ OOP Exercícios/DesafioCampeao/src/Main.java
@@ -1,11 +1,56 @@
-import java.time.LocalDate;
-import java.util.Locale;
+import entities.Champion;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-        Scanner  sc = new Scanner(System.in);
+
+    public static void Main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite os dados do primeiro campeão:");
+        System.out.print("Nome: ");
+        String name1 = sc.nextLine();
+        System.out.print("Vida inicial: ");
+        int life1 = sc.nextInt();
+        System.out.print("Ataque: ");
+        int attack1 = sc.nextInt();
+        System.out.print("Armadura: ");
+        int armor1 = sc.nextInt();
+        sc.nextLine();
+
+        Champion c1 = new Champion(name1, life1, attack1, armor1);
+
+        System.out.println("Digite os dados do segundo campeão:");
+        System.out.print("Nome: ");
+        String name2 = sc.nextLine();
+        System.out.print("Vida inicial: ");
+        int life2 = sc.nextInt();
+        System.out.print("Ataque: ");
+        int attack2 = sc.nextInt();
+        System.out.print("Armadura: ");
+        int armor2 = sc.nextInt();
+
+        Champion c2 = new Champion(name2, life2, attack2, armor2);
+
+        System.out.print("Quantos turnos você deseja executar? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (c1.getLife() == 0 || c2.getLife() == 0) {
+                break;
+            }
+
+            c1.takeDamage(c2);
+            c2.takeDamage(c1);
+
+            System.out.println("Resultado do turno " + i + ":");
+            System.out.println(c1.status());
+            System.out.println(c2.status());
+        }
+
+        System.out.println("FIM DO COMBATE");
 
         sc.close();
     }
--- OOP Exercícios/DesafioCampeao/src/entities/Champion.java
+++ OOP Exercícios/DesafioCampeao/src/entities/Champion.java
@@ -7,16 +7,48 @@ public class Champion {
     private int attack;
     private int armor;
 
-    public Champion(int armor, int attack, int life, String name) {
-        this.armor = armor;
-        this.attack = attack;
-        this.life = life;
+    public Champion(String name, int life, int attack, int armor) {
         this.name = name;
+        this.life = life;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getLife() {
         return life;
     }
 
-    public void takeDamage()
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public void takeDamage(Champion other) {
+        int damage = other.getAttack() - armor;
+        if (damage < 1) {
+            damage = 1;
+        }
+        life -= damage;
+        if (life < 0) {
+            life = 0;
+        }
+    }
+
+    public String status() {
+        if (life == 0) {
+            return name + ": 0 de vida (morreu)";
+        }
+        return name + ": " + life + " de vida";
+    }
 }
